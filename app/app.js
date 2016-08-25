0eb34de09a0ba83c895b2274efa97f3e3f6ce122
GyeongMo Lim <km9173@naver.com>
2016-08-26 00:04:59 +0900

    Remove function ()

--- app/app.js
+++ app/app.js
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(allowCrossDomain());
+app.use(allowCrossDomain);
 app.use('/', tempIndexRouter);
 
 // catch 404 and forward to error handler
