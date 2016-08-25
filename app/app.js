48ba0a30b7acb786e6c628750d24dee1a07f31fc
GyeongMo Lim <km9173@naver.com>
2016-08-26 00:16:22 +0900

    Change Access-control allow origin option

--- app/app.js
+++ app/app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var tempIndexRouter = require('./routes/developBoard');
 
 var allowCrossDomain = function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://api.ooparts.club');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
 
